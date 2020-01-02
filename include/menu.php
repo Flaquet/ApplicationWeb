3e54bfa6d39e5d03385f342e1434654a5a6c8d7e
Flaquet <eflaquet@la-providence.net>
2020-01-02 22:07:20 +0100

    Commit

--- include/menu.php
+++ include/menu.php
@@ -1,10 +1,10 @@
-<link rel="stylesheet" href="../CSS/stylemenu.css">
 <?php 
 
     function Menu(){
        if (!isset($_SESSION['id'])){
             ?>
             <div class='wifeo_conteneur_menu'>
+            <div class='wifeo_pagemenu'><a href="index.php">Page d'accueil</a></div>
                 <div class='wifeo_rubrique'><a href='#mw999'>Inscrip/Connec</a>
                     <div class='wifeo_sousmenu'>
                         <div class='wifeo_pagesousmenu'><a href=inscription.php>Inscription</a></div>
