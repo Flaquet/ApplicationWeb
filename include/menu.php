9d0c3b7ec384c83ff93a3125d52ad0728d999eb9
Flaquet <eflaquet@la-providence.net>
2020-01-02 22:02:35 +0100

    Commit

--- include/menu.php
+++ include/menu.php
@@ -4,13 +4,19 @@
     function Menu(){
        if (!isset($_SESSION['id'])){
             ?>
-            <a href=inscription.php>Inscription</a>
-            <a href="connexion.php">Connexion</a>
+            <div class='wifeo_conteneur_menu'>
+                <div class='wifeo_rubrique'><a href='#mw999'>Inscrip/Connec</a>
+                    <div class='wifeo_sousmenu'>
+                        <div class='wifeo_pagesousmenu'><a href=inscription.php>Inscription</a></div>
+                        <div class='wifeo_pagesousmenu'><a href="connexion.php">Connexion</a></div>
+                    </div>
+                </div>
             <?php
         }else  if(isset($_SESSION['id'])){
             ?>
-            <a href="voteskin.php">Vote pour un Skin</a>
-            <a href="deconnexion.php">deconnexion</a>
+                <div class='wifeo_pagemenu'><a href="voteskin.php">Vote pour un Skin</a></div>
+                <div class='wifeo_pagemenu'><a href="deconnexion.php">deconnexion</a></div>
+            </div>
         <?php
         }  
 
