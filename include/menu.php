0dbc6ddd1d10b8d1023a533d0cd7763d8c59776c
Flaquet <eflaquet@la-providence.net>
2020-01-02 22:21:20 +0100

    Committest

--- include/menu.php
+++ include/menu.php
@@ -12,11 +12,14 @@
                     </div>
                 </div>
                 <div class='wifeo_pagemenu'><a href="Skinavote.php">Skin A Voter</a></div>
+            </div>
             <?php
         }else if(isset($_SESSION['id'])){
             ?>
-                <div class='wifeo_pagemenu'><a href="voteskin.php">Vote pour un Skin</a></div>
-                <div class='wifeo_pagemenu'><a href="deconnexion.php">deconnexion</a></div>
+            <div class='wifeo_conteneur_menu'>
+                <div class='wifeo_pagemenu'><a href="index.php">Page d'accueil</a></div>
+                <div class='wifeo_pagemenu'><a href="voteskin.php">Vote pour un Skin</a></div>   
+                <div class='wifeo_pagemenu'><a href="deconnexion.php">deconnexion</a></div> 
             </div>
         <?php
         }  
