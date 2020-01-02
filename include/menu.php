7fa044a3408600763f0ca4db332cd6cd05d802e2
cdelattre08 <cyril.delattre02@gmail.com>
2020-01-02 21:33:31 +0100

    Commit

--- include/menu.php
+++ include/menu.php
@@ -1,23 +1,18 @@
-
 <link rel="stylesheet" href="../CSS/stylemenu.css">
 <?php 
 
     function Menu(){
-        if (!isset($_SESSION['id'])){
+       // if (!isset($_SESSION['id'])){
             ?>
             <a href=inscription.php>Inscription</a>
             <a href="connexion.php">Connexion</a>
             <?php
-        }else  if(isset($_SESSION['id'])){?>
+        //}else  if(isset($_SESSION['id'])){
 
-            <a href="voteskin.php">Vote pour un Skin</a>
-            <a href="deconnexion.php">deconnexion</a>
+           // <a href="voteskin.php">Vote pour un Skin</a>
+            //<a href="deconnexion.php">deconnexion</a>
         
-        <?php}  ?>
-
-        
-    
-    <?php
+        //}  
 
     }
 
