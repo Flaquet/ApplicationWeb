6deac45fb5e5b41159e9e9d32065c073433c38ad
cdelattre08 <cyril.delattre02@gmail.com>
2020-01-02 23:11:55 +0100

    cyrilcommit3

---
+++ voteskin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<nav>
+                        <div class="button1">
+                            <li><a href="pagegenmiracle.html"> <img src="aerostier.png"> </a></li> </div>
+    
+                            </li>
+                            <div class="button2">
+                            <li><a href="pageseirin.html"> <img src="reconexpert.png"> </a>
+                                </li> </div>
+                                
+                            </li>
+                            <div class="button3">
+                            <li> <a href="pagekaijo.html"> <img src="pioche1"> </a>
+                            </li> </div>
+
+                            <li>
+                            <div class="button4">
+                                <li><a href="pageshutoku.html"> <img src="imgshu"> </a>
+                                </li> </div>
+            
+                                    </li>
+                                    <div class="button5">
+                                    <li><a href="pagetoo.html"> <img src="img.jpg"> </a></li> </div>
+                                        
+                                    </li>
+                                    <div class="button6">
+                                    <li> <a href="pagekaijo.html"> <img src="imgkaijo.png"> </a></li> </div>
+                                        </li>
+                        
+                    </nav>
+    
+</body>
+</html>
