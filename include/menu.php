6afe4d823b9abb235827d964e01e1f1f011198a8
Flaquet <eflaquet@la-providence.net>
2020-01-02 22:18:23 +0100

    Commit

--- include/menu.php
+++ include/menu.php
@@ -4,15 +4,16 @@
        if (!isset($_SESSION['id'])){
             ?>
             <div class='wifeo_conteneur_menu'>
-            <div class='wifeo_pagemenu'><a href="index.php">Page d'accueil</a></div>
+                <div class='wifeo_pagemenu'><a href="index.php">Page d'accueil</a></div>
                 <div class='wifeo_rubrique'><a href='#mw999'>Inscrip/Connec</a>
                     <div class='wifeo_sousmenu'>
                         <div class='wifeo_pagesousmenu'><a href=inscription.php>Inscription</a></div>
                         <div class='wifeo_pagesousmenu'><a href="connexion.php">Connexion</a></div>
                     </div>
                 </div>
+                <div class='wifeo_pagemenu'><a href="Skinavote.php">Skin A Voter</a></div>
             <?php
-        }else  if(isset($_SESSION['id'])){
+        }else if(isset($_SESSION['id'])){
             ?>
                 <div class='wifeo_pagemenu'><a href="voteskin.php">Vote pour un Skin</a></div>
                 <div class='wifeo_pagemenu'><a href="deconnexion.php">deconnexion</a></div>
--- inscription.php
+++ inscription.php
@@ -17,6 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inscription</title>
+    <link rel="stylesheet" href="CSS/stylemenu.css">
 </head>
 <body>
 
