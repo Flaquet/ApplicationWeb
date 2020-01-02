2f49fd1672f6af8c6a0aa608474953a45f5798c0
Flaquet <eflaquet@la-providence.net>
2020-01-02 21:38:51 +0100

    Commit

--- include/menu.php
+++ include/menu.php
@@ -2,17 +2,17 @@
 <?php 
 
     function Menu(){
-       // if (!isset($_SESSION['id'])){
+       if (!isset($_SESSION['id'])){
             ?>
             <a href=inscription.php>Inscription</a>
             <a href="connexion.php">Connexion</a>
             <?php
-        //}else  if(isset($_SESSION['id'])){
-
-           // <a href="voteskin.php">Vote pour un Skin</a>
-            //<a href="deconnexion.php">deconnexion</a>
-        
-        //}  
+        }else  if(isset($_SESSION['id'])){
+            ?>
+            <a href="voteskin.php">Vote pour un Skin</a>
+            <a href="deconnexion.php">deconnexion</a>
+        <?php
+        }  
 
     }
 
