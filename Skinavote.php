a287b1fa6acdd000547ed03c6abaa43ce1f57beb
Flaquet <eflaquet@la-providence.net>
2020-01-05 14:01:15 +0100

    Commit

--- Skinavote.php
+++ Skinavote.php
@@ -18,73 +18,85 @@
         </div>
     </section>
     <section class="container">
-        <div class="container--thicker">
-            <ul class="galery">
-                <li>
-                    <figure>
-                        <img src="IMG/Equipage_du_Chapeau_de_Paille_Jolly_Roger.png" srcset="" alt="" />
-                    </figure>
-                    <span></span>
-                  
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div class="container--thicker">
-            <ul class="galery">
-                <li>
-                    <figure>
-                        <img src="IMG/chevaleresse.png" srcset="" alt="" />
-                    </figure>
-                    <span></span>
-                
-                </li>
-                <li>
-                    <figure>
-                        <img src="IMG/epice.png" srcset="" alt="" />
-                    </figure>
-                    <span></span>
-                  
+        <form method="post">
+            <div class="container--thicker">
+                <ul class="galery">
 
-                </li>
-                <li>
-                    <figure>
-                        <img src="IMG/goule.png" srcset="" alt="" />
-                    </figure>
-                    <span></span>
-                    
-                </li>
-                <li>
-                    <figure>
-                        <img src="IMG/joueuse.png" srcset="" alt="" />
-                    </figure>
-                    <span></span>
-                  
+                    <li>
 
-                </li>
-                <li>
-                    <figure>
-                        <img src="IMG/ikonik.png" srcset="" alt="" />
-                    </figure>
-                    <span></span>
-                   
+                        <figure>
+                            <img src="IMG/chevaleresse.png" srcset="" alt="" />
+                        </figure>
+                        <span></span>
+                        <input type="radio" name="chevaleresse" value="chevaleresse">
 
-                </li>
-                </li>
-                <li>
-                    <figure>
-                        <img src="IMG/lapin.png" srcset="" alt="" />
-                    </figure>
-                    <span></span>
                     </li>
-                <li>
-                    <figure>
-                        <img src="IMG/Dynamo.png" srcset="" alt="" />
-                    </figure>
-                    <span></span>
-            </ul>
-        </div>
+                    <li>
 
+                        <figure>
+                            <img src="IMG/epice.png" srcset="" alt="" />
+                        </figure>
+                        <span></span>
+                        <input type="radio" name="epice" value="epice">
+
+                    </li>
+                    <li>
+
+                        <figure>
+                            <img src="IMG/goule.png" srcset="" alt="" />
+                        </figure>
+                        <span></span>
+                        <input type="radio" name="goule" value="goule">
+
+                    </li>
+                    <li>
+                        <figure>
+                            <img src="IMG/joueuse.png" srcset="" alt="" />
+                        </figure>
+                        <span></span>
+                        <input type="radio" name="joueuse" value="joueuse">
+
+                    </li>
+                    <li>
+
+                        <figure>
+                            <img src="IMG/ikonik.png" srcset="" alt="" />
+                        </figure>
+                        <span></span>
+                        <input type="radio" name="ikonik" value="ikonik">
+
+
+                    </li>
+
+                    <li>
+
+                        <figure>
+                            <img src="IMG/lapin.png" srcset="" alt="" />
+                        </figure>
+                        <span></span>
+                        <input type="radio" name="lapin" value="lapin">
+                    </li>
+                    <li>
+
+                        <figure>
+                            <img src="IMG/Dynamo.png" srcset="" alt="" />
+                        </figure>
+                        <span></span>
+                        <input type="radio" name="Dynamo" value="Dynamo">
+
+                    </li>
+
+
+                </ul>
+            </div>
+            <div class="container--thicker">
+                <ul class="galery">
+                    <li>
+                        <input type="submit" value="Ok !">
+                    </li>
+                </ul>
+            </div>
+        </form>
     </section>
 
 
